2a41bc14d60b75e1597e902ae46e7cfb6b37841d
Rabab <rababahmed6@gmail.com>
2021-05-12 04:13:45 +0600

    heroku

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,6 +3,10 @@ import { PrismaClient } from "@prisma/client";
 import express from "express";
 import { buildSchema } from "type-graphql";
 import { ApolloServer } from "apollo-server-express";
+import { resolvers } from "./generated/typegraphql-prisma/index";
+interface Context {
+  prisma: PrismaClient;
+}
 
 const PORT = process.env.PORT || 4000;
 
@@ -12,6 +16,17 @@ const main = async () => {
   const app = express();
 
   await prisma.$connect();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers,
+      validate: false,
+    }),
+    playground: true,
+    context: (): Context => ({ prisma }),
+  });
+
+  apolloServer.applyMiddleware({ app });
 
   app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
