b758ded0bcd81a47e43ed012ea8f56efcb7b9b2c
Rabab <rababahmed6@gmail.com>
2021-05-13 23:35:21 +0600

    heroku

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,8 +5,6 @@ import { buildSchema } from "type-graphql";
 import { ApolloServer } from "apollo-server-express";
 import { resolvers } from "../prisma/generated/type-graphql/";
 const passport = require("passport");
-const bcrypt = require("bcryptjs");
-const LocalStrategy = require("passport-local").Strategy;
 
 const bodyParser = require("body-parser");
 const userRouter = require("./routes/UserAuth");
@@ -39,45 +37,6 @@ const main = async () => {
   apolloServer.applyMiddleware({ app });
 
   app.use("/user", userRouter);
-
-  app.post(
-    "/log-in",
-    passport.authenticate("local", {
-      successRedirect: "/",
-      failureRedirect: "/",
-    })
-  );
-
-  passport.use(
-    new LocalStrategy(async (email, password, done) => {
-      const user = await prisma.user.findUnique({
-        where: {
-          email: email,
-        },
-      });
-      if (!user) {
-        return done(null, false, { message: "Incorrect username" });
-      }
-      if (
-        bcrypt.compare(password, user.password, (err, res) => {
-          if (res) {
-            // passwords match! log user in
-            return done(null, user);
-          } else {
-            // passwords do not match!
-            return done(null, false, { message: "Incorrect password" });
-          }
-        })
-      ) {
-        return done(null, false, { message: "Incorrect password" });
-      }
-      return done(null, user);
-    })
-  );
-
-  passport.serializeUser(function (user, done) {
-    done(null, user.id);
-  });
 
   app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
