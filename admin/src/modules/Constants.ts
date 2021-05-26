8250a1126194a5d326c7d6922d81696b20ee354f
Rabab <rababahmed6@gmail.com>
2021-05-27 02:26:24 +0600

    refactor: move api keys to env variables

--- admin/src/modules/Constants.ts
+++ admin/src/modules/Constants.ts
@@ -1,1 +1,2 @@
-export const GraphQL_API = "https://tezzbites-api.herokuapp.com/graphql";
+export const GraphQL_API = process.env.NEXT_PUBLIC_GQL_ENDPOINT;
+export const REST_API_V1 = process.env.NEXT_PUBLIC_REST_ENDPOINT_V1;
