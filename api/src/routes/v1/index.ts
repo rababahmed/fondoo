b8a1733b729e34bd79c47b56b1109acbed8a7a88
Rabab Ahmed <rababahmed6@gmail.com>
2021-07-08 03:41:09 +0600

    feat: add restaurant basic REST route

--- api/src/routes/v1/index.ts
+++ api/src/routes/v1/index.ts
@@ -2,10 +2,12 @@ import * as express from "express";
 
 const userAuthRouter = require("./UserAuth");
 const uploadRouter = require("./UploadRouter");
+const restaurantRouter = require("./restaurantRouter");
 
 let v1Router = express.Router();
 
 v1Router.use("/user", userAuthRouter);
 v1Router.use("/uploads", uploadRouter);
+v1Router.use("/restaurant", restaurantRouter);
 
 export default v1Router;
---
+++ api/src/routes/v1/restaurantRouter.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import prisma from "../../PrismaClient";
+
+const router = express.Router();
+
+router.get("/:domain", async (req, res) => {
+  try {
+    const { domain } = req.params;
+
+    const result = await prisma.restaurantConfig.findUnique({
+      where: { domain: domain },
+      include: { Restaurant: true },
+    });
+
+    if (result) {
+      res.status(200).send({ data: result });
+    } else {
+      res.status(400).send({ message: "Restaurant not found" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(400).send({ message: "Error creating user" });
+  }
+});
+
+module.exports = router;
