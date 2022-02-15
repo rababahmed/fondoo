d8f52ea1f53d572bda82556dde0bd32be8377241
Rabab Ahmed <rababahmed6@gmail.com>
2022-02-15 21:52:51 +0600

    fix(marketing): add plausible wrapper

--- marketing/next.config.js
+++ marketing/next.config.js
@@ -1,7 +1,9 @@
+const { withPlausibleProxy } = require("next-plausible");
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withPlausibleProxy()({
   reactStrictMode: true,
   images: {
     domains: ["images.prismic.io", "fondoo.cdn.prismic.io"],
   },
-};
+});
