1e25487054cf2a7e2f75c5ea28602c1aa91efcdb
Rabab Ahmed <rababahmed6@gmail.com>
2022-02-19 17:35:34 +0600

    feat(marketing): add btw chatbot

--- marketing/src/components/Analytics.tsx
+++ marketing/src/components/Analytics.tsx
@@ -29,6 +29,16 @@ const Analytics = () => {
           data-api="https://odd-snowflake-bde9.rabab.workers.dev/odd-snowflake/event"
           strategy="afterInteractive"
         />
+        <Script
+          id="btw-widget"
+          data-project-code="776a5c10-b3fb-41a8-ae64-d33d8831f954"
+          data-iframe-domain="https://app.btw.so"
+          data-conversations-domain="https://conversations.btw.so"
+          data-events-domain="https://events.btw.so"
+          data-events-secret="js.nti5bhj8o8nqaikp1k4fb.kkvwbs6pqqj2jgkksmodvw"
+          src="https://conversations.btw.so/widget/dist/btw.js"
+          strategy="afterInteractive"
+        />
       </div>
     </>
   );
