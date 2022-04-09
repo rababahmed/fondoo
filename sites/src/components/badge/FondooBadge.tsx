927fc16629631c1ecc130e3500c37032c6abb45a
Rabab Ahmed <rababahmed6@gmail.com>
2022-04-10 01:32:45 +0600

    feat(sites): add fondoo badge

---
+++ sites/src/components/badge/FondooBadge.tsx
@@ -0,0 +1,41 @@
+import { Box, HStack, Link, Text } from "@chakra-ui/react";
+import Image from "next/image";
+import React from "react";
+import FondooLogo from "../../../public/images/fondoo-logo.png";
+
+const FondooBadge = () => {
+  return (
+    <Link href="https://www.fondoo.io" isExternal>
+      <Box pos={"fixed"} bottom="0" right="0" p={4} zIndex={20}>
+        <Box
+          py={2}
+          px={2}
+          display="flex"
+          alignItems={"center"}
+          bg={"black"}
+          rounded="lg"
+          shadow="sm"
+        >
+          <Image
+            src={FondooLogo}
+            alt="Fondoo Logo"
+            width={24}
+            height={24}
+            layout="intrinsic"
+          />
+          <Text
+            color="white"
+            fontSize={"sm"}
+            fontWeight={"normal"}
+            fontFamily="TT Norms Pro Medium"
+            pr={2}
+          >
+            Powered by Fondoo
+          </Text>
+        </Box>
+      </Box>
+    </Link>
+  );
+};
+
+export default FondooBadge;
